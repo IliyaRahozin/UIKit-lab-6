d7c7ae1d57153e269eb34bf7d6a1f842a6a13e96
YARUSHA0NfIRE <yarusha4true@gmail.com>
2023-07-18 19:48:05 +0300

    no message

--- UIKit-lab-6/UIKit-lab-6/ViewController.swift
+++ UIKit-lab-6/UIKit-lab-6/ViewController.swift
@@ -15,14 +15,13 @@ class ViewController: UIViewController {
     private lazy var square: UIView = {
         let view = UIView(frame: CGRect(x: 150, y: 300, width: 100, height: 100))
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .systemIndigo
+        view.backgroundColor = .systemBlue
         view.layer.cornerRadius = 10
         return view
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray6
         view.addSubview(square)
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
